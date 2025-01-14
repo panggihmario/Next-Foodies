07bb7ccd2c89171de27a4a25e98719acc3b14a9c
panggih mario <panggihmario@gmail.com>
2025-01-14 08:48:43 +0700

    setup loading and eror page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,34 @@
-export default function MealsPage() {
-  return <h1>Meals</h1>;
+import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "../components/meals/meals-grid";
+import { getMeal } from "@/lib/meals";
+import { Suspense } from "react";
+import Loading from "./loading-out.js";
+async function Meals() {
+  const meals = await getMeal();
+  return <MealsGrid meals={meals} />;
+}
+
+export default async function MealsPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+          <p>
+            Choose your favorite recipe and cook it yourself. It is easy and fun
+          </p>
+          <p className={classes.cta}>
+            <Link href="/meals/share">Share your favorite meals</Link>
+          </p>
+        </h1>
+        <main className={classes.main}>
+          <Suspense fallback={<Loading />}>
+            <Meals />
+          </Suspense>
+        </main>
+      </header>
+    </>
+  );
 }
