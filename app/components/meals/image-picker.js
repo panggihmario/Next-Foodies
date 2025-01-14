19cdd69890b41adc17f075dd0747ecef238e8be2
panggih mario <panggihmario@gmail.com>
2025-01-14 10:49:58 +0700

    add image picker

--- app/components/meals/image-picker.js
+++ app/components/meals/image-picker.js
@@ -12,6 +12,7 @@ export default function ImagePicker({ label, name }) {
   function handleChangeImage(event) {
     const file = event.target.files[0];
     if (!file) {
+      setPickedImage(null);
       return;
     }
     const fileReader = new FileReader();
