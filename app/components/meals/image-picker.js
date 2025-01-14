16edf625baf3928d50171d81df68a2b8e5531219
panggih mario <panggihmario@gmail.com>
2025-01-14 10:23:11 +0700

    add image picker

---
+++ app/components/meals/image-picker.js
@@ -0,0 +1,49 @@
+"use client";
+import Image from "next/image";
+import classes from "./image-picker.module.css";
+import { useRef, useState } from "react";
+export default function ImagePicker({ label, name }) {
+  const imageRef = useRef();
+  const [pickedImage, setPickedImage] = useState();
+  function handleClick() {
+    imageRef.current.click();
+  }
+
+  function handleChangeImage(event) {
+    const file = event.target.files[0];
+    if (!file) {
+      return;
+    }
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  }
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet</p>}
+          {pickedImage && (
+            <Image src={pickedImage} fill alt="The image selected by user" />
+          )}
+        </div>
+        <input
+          type="file"
+          className={classes.input}
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={imageRef}
+          onChange={handleChangeImage}
+        />
+        <button onClick={handleClick} className={classes.button} type="button">
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
+}
--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,12 @@
 import classes from "./page.module.css";
 import Image from "next/image";
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 export default function MealsDetailPage({ params }) {
   const meal = getMeal(params.mealSlug);
+  if (!meal) {
+    notFound();
+  }
   meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
   return (
     <>
